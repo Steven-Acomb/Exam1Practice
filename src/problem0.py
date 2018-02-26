323be6d9fe1f7afbc16d29635cf219953f5c9db6
acombsr <acombsr@rose-hulman.edu>
2018-02-25 22:10:16 -0500

    completed problem 0

--- src/problem0.py
+++ src/problem0.py
@@ -8,16 +8,17 @@ These problems illustrate concepts that previous problems have not emphasized:
   -- animation (Problem 0c)
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Stephen Acomb.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
+import math
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_problem0a()
-    run_test_problem0b()
+    #run_test_problem0a()
+    #run_test_problem0b()
     run_test_problem0c()
 
 
@@ -146,7 +147,7 @@ def problem0a(n):
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -163,6 +164,12 @@ def problem0a(n):
     #        Simply try a few examples to convince yourself of this.
     #        ASK FOR HELP if you do not understand this hint.
     # ------------------------------------------------------------------
+    sum = 0
+    for k in range(int(math.log(n, 10))):
+        sum += n%(10^(int(math.log(n,10))-k))
+    if(sum%2 == 1):
+        return True
+    return False
 
 
 def run_test_problem0b():
@@ -218,7 +225,7 @@ def problem0b(n):
            since there are 46 primes between 2 and 200.
      """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -227,6 +234,11 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(n-1):
+        if(is_prime(k+2)):
+            count +=1
+    return count
 
 
 def run_test_problem0c():
@@ -286,7 +298,7 @@ def problem0c(circle, n, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -294,7 +306,13 @@ def problem0c(circle, n, window):
     #   renders with a half-second pause after rendering.
     ####################################################################
     # ------------------------------------------------------------------
-
+    circle.attach_to(window)
+    for k in range(n):
+        center2 = rg.Point(circle.center.x + k*2*circle.radius,
+                           circle.center.y)
+        circle2 = rg.Circle(center2, circle.radius)
+        circle2.attach_to(window)
+        window.render(.5)
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
